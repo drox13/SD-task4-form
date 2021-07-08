a65f71124e27696d135edc6cdf2cab80798aa0ca
drox13 <dario.baron@uptc.edu.co>
2021-07-08 16:22:28 -0500

    se envian los datos al servidor y este nos resonde con la lista actualizada

--- client.js
+++ client.js
@@ -1,33 +1,11 @@
 "use strict"
 var button = document.querySelector("#btn");
-button.addEventListener("click", sendJSON, true);
+button.addEventListener("click", send, true);
 
-var data = document.querySelector("#data");
-function paintList(){
+function send(){
     let xhr = new XMLHttpRequest();
     let url = "localhost:5000/";
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var p = data.appendChild("p");
-            p.innerHTML = this.responseText;
-        }
-    };
-    xhr.open("GET", url, true);
-}
-
-function sendJSON(){
-    console.log("se oprimio btn");
-    var product = document.querySelector("#product").value;
-    var price = document.querySelector("#priceId").value;
-
-    let xhr = new XMLHttpRequest();
-    let url = "localhost:5000/";
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            console.log(JSON.parse(this.responseText));
-        }
-    };
+    
     xhr.open("POST", url, true);
-    var data = JSON.stringify({ "product": product, "price": price });
-    xhr.send(data);
+    xhr.send();
 }
--- server.js
+++ server.js
@@ -11,28 +11,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 var productList = [
-    {"product": "producto prueba", "price": 20},
+    {"product": "producto prueba", "price": 20}
 ];
-/*
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    //res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Access-Control-Allow-Credentials', true);
-    
-    res.header("Access-Control-Allow-Headers", 'content-type:application/json');
-    next();
-});
-*/
 
 app.get('/', function (req, res) {
     res.sendFile('index.html', { root: __dirname });
 })
 
 app.post('/element', function (req, res) {
-    console.log(req.body.price);
-    var product = {"product:": req.body.product, "price": req.body.price};
+    console.log(req);
+    var product = {"product:": req.body.product, "price": parseInt(req.body.price) };
     productList.push(product);
     res.send(productList);
 })
